ef5a636c945ddbca5f95596ad06762d8c6adf527
Sergio Rubio <rubiojr@frameos.org>
2012-11-22 19:15:52 +0100

    Do not try to use newer Swift3 middleware packages and config in Precise
    swift-plugin-s3 only available in Quantal an newer versions, maybe in
    Folsom backports too.

--- manifests/proxy/swift3.pp
+++ manifests/proxy/swift3.pp
@@ -20,9 +20,13 @@ class swift::proxy::swift3(
 
   include $::swift::params
 
-  package { 'swift-plugin-s3':
-    name   => $::swift::params::swift3,
-    ensure => $ensure,
+  # swift-plugin-s3 package not available in Precise/Essex
+  # http://packages.ubuntu.com/search?keywords=swift-plugin-s3
+  if $::lsbdistcodename != 'precise' {
+    package { 'swift-plugin-s3':
+      name   => $::swift::params::swift3,
+      ensure => $ensure,
+    }
   }
 
   concat::fragment { 'swift_swift3':
